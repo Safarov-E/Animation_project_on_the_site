1af4a3a82d7d29aef80b26127ed29b2c765b129d
Elkhan <elkhan.safarov10@yandex.ru>
2019-03-12 23:50:08 +0500

    отправки данных с формы

--- js/script.js
+++ js/script.js
@@ -87,4 +87,44 @@ window.addEventListener('DOMContentLoaded', function (){
 	 		more.classList.remove('more-splash');
 	 		document.body.style.overflow = '';
 	 	});
+
+	 	let message = new Object();
+	 	message.loading = "Загрузка...";
+	 	message.success = "Спасибо! Скоро мы с вами свяжемся";
+	 	message.failure = "Что-то пошло не так...";
+
+	 	let form = document.getElementsByClassName('main-form')[0],
+	 		input = form.getElementsByClassName('input'),
+	 		statusMessage = document.createElement('div');
+	 		statusMessage.classList.add('status');
+
+	 		form.addEventListener('submit', function(event){
+	 			event.preventDefault();
+	 			form.appendChild(statusMessage);
+
+	 			let request = new XMLHtttpRequest();
+	 			request.open("POST", 'server.php')
+
+	 			request.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+
+	 			let formData = new FormData(form);
+
+	 			request.send(formData);
+
+	 			request.onreadystatechange = function(){
+	 				if (request.readyState < 4) {
+	 					statusMessage.innerHTML = message.loading;
+	 				} else if (request.readyState === 4) {
+	 					if (request.status == 200 && request.status < 300) {
+	 						statusMessage.innerHTML = message.success;
+	 					}
+	 					else {
+	 						statusMessage.innerHTML = message.failure;
+	 					}
+	 				}
+	 			}
+	 			for (var i = 0; i < input.length; i++) {
+	 				input[i].value = '';
+	 			}
+	 		});
 });
